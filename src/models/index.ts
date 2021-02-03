f88db9c8fbf0072c3d5aa2614a9aa71e671ca2e5
franfiori29 <franfiori@gmail.com>
2021-02-03 17:40:26 -0300

    dependencies

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,41 +1,44 @@
-import { Sequelize, DataTypes, Model, BuildOptions   } from 'sequelize';
+import { Sequelize, DataTypes, Model, BuildOptions } from "sequelize";
 
-export const db = new Sequelize('postgres://postgres:asdasd123@localhost:5432/recipeBack', {
-	logging: false,
-});
+export const db = new Sequelize(
+	"postgres://postgres:asdasd123@localhost:5432/recipeBack",
+	{
+		logging: false,
+	}
+);
 
-export  interface RecipeAttributes{
-	id: number,
-	title: string,
-	likes: number,
-	image: string,
-	createdAt?: Date,
-  	updatedAt?: Date,
+export interface RecipeAttributes {
+	id: number;
+	title: string;
+	likes: number;
+	image: string;
+	createdAt?: Date;
+	updatedAt?: Date;
 }
 
-export interface RecipeModel extends Model<RecipeAttributes>, RecipeAttributes {}
+export interface RecipeModel
+	extends Model<RecipeAttributes>,
+		RecipeAttributes {}
 
 export type RecipeStatic = typeof Model & {
-    new (values?: object, options?: BuildOptions): RecipeModel;
+	new (values?: object, options?: BuildOptions): RecipeModel;
 };
 
-
-
-export function RecipeFactory (sequelize:Sequelize): RecipeStatic {
-return <RecipeStatic>sequelize.define('recipe', {
-	title: {
-		type: DataTypes.STRING,
-		allowNull: false
-	},
-	likes: {
-		type: DataTypes.INTEGER,
-		allowNull: false
-	},
-	image: {
-		type: DataTypes.STRING,
-		allowNull: false
-	}
-})
+export function RecipeFactory(sequelize: Sequelize): RecipeStatic {
+	return <RecipeStatic>sequelize.define("recipe", {
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		likes: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		image: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	});
 }
 
 export const Recipe = RecipeFactory(db);
