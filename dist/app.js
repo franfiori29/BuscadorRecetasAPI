1a75cbc797327d1d68e552e46f54a1bd58aa2393
franfiori29 <franfiori@gmail.com>
2021-02-03 23:08:33 -0300

    plis

--- dist/app.js
+++ dist/app.js
@@ -21,7 +21,7 @@ app.use(body_parser_1.default.json({ limit: "50mb" }));
 app.use((_, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const models_1 = require("./models");
 const app_js_1 = __importDefault(require("./app.js"));
+require("dotenv").config();
+const { PORT } = process.env;
 const force = false;
-models_1.db.sync({ force })
-    .then(function () {
-    app_js_1.default.listen(4000, function () {
-        console.log('Servidor funcionando');
+models_1.db.sync({ force }).then(function () {
+    app_js_1.default.listen(PORT, function () {
+        console.log("Servidor funcionando");
     });
 });
 //# sourceMappingURL=index.js.map
