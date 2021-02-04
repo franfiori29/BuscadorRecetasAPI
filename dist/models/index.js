87b682f9e292685d173c80f15a9ebcf145185d20
franfiori29 <franfiori@gmail.com>
2021-02-03 22:32:34 -0300

    variables db

--- dist/models/index.js
+++ dist/models/index.js
@@ -2,23 +2,45 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Recipe = exports.RecipeFactory = exports.db = void 0;
 const sequelize_1 = require("sequelize");
-exports.db = new sequelize_1.Sequelize('postgres://postgres:asdasd123@localhost:5432/recipeBack', {
-    logging: false,
-});
+require("dotenv").config();
+const { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD } = process.env;
+exports.db = process.env.NODE_ENV === "production"
+    ? new sequelize_1.Sequelize({
+        database: DB_NAME,
+        dialect: "postgres",
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+            max: 3,
+            min: 1,
+            idle: 10000,
+        },
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+            keepAlive: true,
+        },
+        ssl: true,
+    })
+    : new sequelize_1.Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, { logging: false, native: false });
 function RecipeFactory(sequelize) {
-    return sequelize.define('recipe', {
+    return sequelize.define("recipe", {
         title: {
             type: sequelize_1.DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
         },
         likes: {
             type: sequelize_1.DataTypes.INTEGER,
-            allowNull: false
+            allowNull: false,
         },
         image: {
             type: sequelize_1.DataTypes.STRING,
-            allowNull: false
-        }
+            allowNull: false,
+        },
     });
 }
 exports.RecipeFactory = RecipeFactory;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,11 +1,35 @@
 import { Sequelize, DataTypes, Model, BuildOptions } from "sequelize";
+require("dotenv").config();
+const { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD } = process.env;
 
-export const db = new Sequelize(
-	"postgres://postgres:asdasd123@localhost:5432/recipeBack",
-	{
-		logging: false,
-	}
-);
+export const db =
+	process.env.NODE_ENV === "production"
+		? new Sequelize({
+				database: DB_NAME,
+				dialect: "postgres",
+				host: DB_HOST,
+				port: 5432,
+				username: DB_USER,
+				password: DB_PASSWORD,
+				pool: {
+					max: 3,
+					min: 1,
+					idle: 10000,
+				},
+				dialectOptions: {
+					ssl: {
+						require: true,
+						// Ref.: https://github.com/brianc/node-postgres/issues/2009
+						rejectUnauthorized: false,
+					},
+					keepAlive: true,
+				},
+				ssl: true,
+		  })
+		: new Sequelize(
+				`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+				{ logging: false, native: false }
+		  );
 
 export interface RecipeAttributes {
 	id: number;
