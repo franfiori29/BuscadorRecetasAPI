2ffe1af0cc47bfd444b3d55c7430f91ca23527ff
franfiori29 <franfiori@gmail.com>
2021-02-03 22:58:22 -0300

    variable PORT

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use((_: Request, res: Response, next) => {
 	res.header("Access-Control-Allow-Credentials", "true");
 	res.header(
 		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
 	);
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 	next();
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { db }  from './models';
-import app from './app.js';
-
+import { db } from "./models";
+import app from "./app.js";
+require("dotenv").config();
+const { PORT } = process.env;
 const force = false;
-db.sync({ force })
-	.then(function () {
-		app.listen(4000, function () {
-			console.log('Servidor funcionando');
-		});
+db.sync({ force }).then(function () {
+	app.listen(PORT, function () {
+		console.log("Servidor funcionando");
 	});
+});
